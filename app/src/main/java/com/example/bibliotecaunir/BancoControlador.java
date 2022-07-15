741a616b4b532af0ce61a6717d4b0e6b61c7e978
adenhercules <adenhercules07@gmail.com>
2022-07-15 10:56:09 -0400

    finalizado

--- app/src/main/java/com/example/bibliotecaunir/BancoControlador.java
+++ app/src/main/java/com/example/bibliotecaunir/BancoControlador.java
@@ -1,12 +1,17 @@
 package com.example.bibliotecaunir;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BancoControlador extends SQLiteOpenHelper {
 
@@ -19,7 +24,7 @@ public class BancoControlador extends SQLiteOpenHelper {
                     + "ano INTEGER);";
 
     public BancoControlador(@Nullable Context contex) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(contex, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
@@ -31,5 +36,68 @@ public class BancoControlador extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS catalogo");
         onCreate(sqLiteDatabase);
+    }
+
+    public long inserir(ContentValues cv){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long id = db.insert("catalogo", null, cv);
+        return id;
+    }
+
+    public List<ContentValues> pesquisarPorTitulo(String titulo){
+        String sql = "SELECT * FROM catalogo WHERE titulo LIKE ?";
+        String where[] = new String[]{"%"+titulo+"%"};
+        return pesquisar(sql, where);
+    }
+
+    public List<ContentValues> pesquisarPorAno(int ano){
+        String sql = "SELECT * FROM catalogo WHERE ano LIKE ?";
+        String where[] = new String[]{String.valueOf(ano)};
+        return pesquisar(sql, where);
+    }
+
+    public List<ContentValues> pesquisarPorTodos(){
+        String sql = "SELECT * FROM catalogo ORDER BY id";
+        String where[] = null;
+        return pesquisar(sql, where);
+    }
+
+    private List<ContentValues> pesquisar(String sql, String where[]){
+        List<ContentValues> lista = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(sql, where);
+
+        if (c.moveToFirst()){
+            do {
+                ContentValues cv = new ContentValues();
+                cv.put("id", c.getInt(c.getColumnIndex("id")));
+                cv.put("titulo", c.getString(c.getColumnIndex("titulo")));
+                cv.put("autor", c.getString(c.getColumnIndex("autor")));
+                cv.put("ano", c.getInt(c.getColumnIndex("ano")));
+                lista.add(cv);
+            }while(c.moveToNext());
+        }
+        return lista;
+    }
+
+    public void alterarRegistro(int id, String titulo, String autor, int ano){
+        ContentValues valores = new ContentValues();
+        String where;
+        SQLiteDatabase db = this.getWritableDatabase();
+        where = "id="+ id;
+        valores.put("titulo", titulo);
+        valores.put("autor", autor);
+        valores.put("ano", ano);
+        db.update("catalogo", valores, where, null);
+        db.close();
+
+    }
+
+    public void deletarRegistro(int id){
+        String where = "id" + id;
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.delete("catalogo", where, null);
+        db.close();
+
     }
 }
--- app/src/main/java/com/example/bibliotecaunir/Pesquisa.java
+++ app/src/main/java/com/example/bibliotecaunir/Pesquisa.java
@@ -2,13 +2,77 @@ package com.example.bibliotecaunir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pesquisa extends AppCompatActivity {
+
+    private Button btnVoltar;
+    private TableLayout tabela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa);
+
+        btnVoltar = findViewById(R.id.btn_voltar_pesquisar);
+        tabela = findViewById(R.id.tb_pesqusia);
+
+        Intent intent = getIntent();
+        int tipo = intent.getIntExtra("tipo", 0);
+        String busca = intent.getStringExtra("busca");
+        List<ContentValues> lista = new ArrayList<>();
+        BancoControlador bd = new BancoControlador(getApplicationContext());
+
+        if(tipo == R.id.rb_titulo){
+            lista = bd.pesquisarPorTitulo(busca);
+        } else if (tipo == R.id.rb_ano){
+            lista = bd.pesquisarPorAno(Integer.parseInt(busca));
+        } else if (tipo == R.id.rb_todos){
+            lista = bd.pesquisarPorTodos();
+        }
+
+        if(lista != null && lista.size() > 0){
+            for(ContentValues cv : lista){
+                TableRow tr = new TableRow(getApplicationContext());
+                TextView col1 = new TextView(getApplicationContext());
+                col1.setText(String.valueOf(cv.getAsInteger("id")));
+                tr.addView(col1);
+
+                TextView col2 = new TextView(getApplicationContext());
+                col2.setText(cv.getAsString("titulo"));
+                tr.addView(col2);
+
+                TextView col3 = new TextView(getApplicationContext());
+                col3.setText(cv.getAsString("autor"));
+                tr.addView(col3);
+
+                TextView col4 = new TextView(getApplicationContext());
+                col4.setText(String.valueOf(cv.getAsInteger("ano")));
+                tr.addView(col4);
+                tr.setPadding(24, 8, 8, 8);
+
+                tabela.addView(tr);
+            }
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
